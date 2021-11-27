c5ec3a2ed16b6a11c32d03761088454bb3039405
igara <igara1119@gmail.com>
2021-11-27 16:32:33 +0900

    modify: test.

--- components/pages/index/__tests__/page/page.test.tsx
+++ components/pages/index/__tests__/page/page.test.tsx
@@ -10,7 +10,7 @@ describe('Page確認', () => {
   test('初期表示', () => {
     const renderTest = TestingLibrary.render(<Page />);
     expect(
-      '<div><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><nav><i class="css-15ch41a-styles-styles">c</i><div>ddd</div></nav><div><header>syonet</header><main>aaa</main><footer>© syonet.work. All Rights Reserved.</footer></div></div></div>',
+      '<div><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><nav><i class="css-fsfbn2">c</i><div>ddd</div></nav><div><header>syonet</header><main>aaa</main><footer>© syonet.work. All Rights Reserved.</footer></div></div></div>',
     ).toBe(renderTest.baseElement.innerHTML);
   });
 });
