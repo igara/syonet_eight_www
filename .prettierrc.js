2a4856f5beaacf9f60e2a6a1ae102ef3b37e0585
igara <igara1119@gmail.com>
2021-12-20 02:11:15 +0900

    modify: rc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,5 @@
+const config = require('../../.prettierrc.js');
+
 module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 90,
-  tabWidth: 2,
-  jsxBracketSameLine: true,
-  endOfLine: 'auto',
+  ...config,
 };
