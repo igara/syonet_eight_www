75262ea2d8b023c51957755e2333d95f643f9bce
igara <igara1119@gmail.com>
2021-12-25 18:21:08 +0900

    modify: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,9 @@
-const path = require('path');
+const config = require('../../jest.config.js');
 
 module.exports = {
-  roots: ['<rootDir>'],
-  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
-  testMatch: ['**/__tests__/**/*.*(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': [
-      'esbuild-jest',
-      {
-        sourcemap: 'both',
-      },
-    ],
-  },
-  testEnvironment: 'jsdom',
-  moduleDirectories: ['node_modules'],
+  ...config,
   moduleNameMapper: {
     '^@www/(.+)': '<rootDir>/$1',
-    '\\.(svg)$': '<rootDir>/../../__mocks__/svg_mock.js',
-    '\\.(jpg|jpeg|png|gif)$': '<rootDir>/../../__mocks__/image_mock.js',
-    '\\.(css)$': '<rootDir>/../../__mocks__/style_mock.js',
+    ...config.moduleNameMapper,
   },
-  globals: {
-    'esbuild-jest': {
-      tsconfig: path.join(__dirname, 'jest.tsconfig.json'),
-      diagnostics: false,
-    },
-  },
-  snapshotSerializers: ['@emotion/jest/serializer'],
-  collectCoverageFrom: ['**/*.{ts|tsx}', '!**/node_modules/**'],
 };
