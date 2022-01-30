2fa0b4e7d172fe224805d1c8ae027a1063ed20c2
igara <igara1119@gmail.com>
2022-01-31 00:41:27 +0900

    change: variables.

--- src/hooks/swr/qiita_list.hooks.ts
+++ src/hooks/swr/qiita_list.hooks.ts
@@ -2,22 +2,7 @@ import useSWR from 'swr';
 import { useSWRByURL } from 'syonet_eight_design_system';
 import * as Variables from 'syonet_eight_variables';
 
-export type QiitaList = {
-  name: string;
-  path: string;
-  sha: string;
-  size: number;
-  url: string;
-  html_url: string;
-  git_url: string;
-  download_url: string | null;
-  type: string;
-  _links: {
-    self: string;
-    git: string;
-    html: string;
-  };
-}[];
+export type QiitaList = Variables.BackupQiitaList;
 
 export const useSWRQiitaListFetch = () => {
   const swrQiita = useSWRByURL<QiitaList, any>({
