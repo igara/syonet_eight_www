3bfbe266c7a6b19933c697089395dc14ef73f8f5
igara <igara1119@gmail.com>
2022-03-29 01:47:52 +0900

    modify: ga.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,11 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends NextDocument {
   render() {
     const ga = `
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}");
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
 `;
 
     const datadogBrowserLogs = `
@@ -57,9 +56,6 @@ DD_LOGS.onReady(function() {
 
           {process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
             <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_MANAGER_ID}`}></script>
               <script dangerouslySetInnerHTML={{ __html: ga }}></script>
             </>
           )}
